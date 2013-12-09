603823c16fb07ea9eeaa4d03bf5ae3baea82f473
Adam Tait <adam@adamtait.com>
2013-12-08 18:36:20 -0800

    Initial Commit

---
+++ TipCalculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TipCalculator
+//
+//  Created by Adam Tait on 12/8/13.
+//  Copyright (c) 2013 Adam Tait. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "nocoAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([nocoAppDelegate class]));
+    }
+}
---
+++ TipCalculator/nocoAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  nocoAppDelegate.h
+//  TipCalculator
+//
+//  Created by Adam Tait on 12/8/13.
+//  Copyright (c) 2013 Adam Tait. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface nocoAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
