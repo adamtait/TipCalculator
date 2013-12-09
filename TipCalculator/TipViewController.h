e6a75e4d2fda77ae845a434d2cb4f3e67628292c
Adam Tait <adam@adamtait.com>
2013-12-08 21:01:47 -0800

    introduction to iOS programming video completed & simple tip calculator implemented

---
+++ TipCalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  TipCalculator
+//
+//  Created by Adam Tait on 12/8/13.
+//  Copyright (c) 2013 Adam Tait. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
